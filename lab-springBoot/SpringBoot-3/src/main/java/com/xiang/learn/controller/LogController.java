bc3bc7c6ceca39a247607c150181af21d8db65d0
wilbur <wkc120796@163.com>
2021-07-27 17:59:27 +0800

    springboot learn lab-3

---
+++ lab-springBoot/SpringBoot-3/src/main/java/com/xiang/learn/controller/LogController.java
@@ -0,0 +1,19 @@
+package com.xiang.learn.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@Slf4j
+public class LogController {
+
+    @RequestMapping("/log")
+    public void logIndex(){
+        log.trace("Trace 日志...");
+        log.debug("Debug 日志...");
+        log.info("Info 日志...");
+        log.warn("Warn 日志...");
+        log.error("Error 日志...");
+    }
+
+}
